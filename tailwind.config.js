d1cd551b7fc0b680c61c67d238404f8c50f08a40
DJANFAROU <hanifdjanfarou8@gmail.com>
2025-01-08 18:41:39 +0000

    first page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./page1/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': 'white',
+        'button': '#f59221',
+        'nav-color': 'rgb(248 250 252 / 0.45)'
+      },
+      minWidth: {
+        "min-screen": "100vh"
+      },
+      backgroundColor: {
+        "btn": "#f59221"
+      }
+      
+    },
   },
   plugins: [],
 };
